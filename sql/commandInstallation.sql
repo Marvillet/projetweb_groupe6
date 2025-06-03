6540d9b8e2bdf86566d15c3073062040398f5839
Mathis Letellier <mathis.letellier@isen-ouest.yncrea.fr>
2025-06-03 16:54:15 +0200

    index

--- sql/commandInstallation.sql
+++ sql/commandInstallation.sql
@@ -1,7 +1,11 @@
 #-- executer tous les autres sql avant
 INSERT INTO panneaux_marque(panneau_marque) SELECT distinct panneaux_marque from mytable;
+CREATE INDEX panneaux_marque_index
+    ON panneaux_marque(panneau_marque);
 
 insert into panneaux_modele(panneau_modele) SELECT DISTINCT mytable.panneaux_modele from mytable;
+CREATE INDEX panneaux_modele_index
+    ON panneaux_modele(panneau_modele);
 
 insert into panneau(id_panneau_modele, id_panneau_marque)
     SELECT DISTINCT modele.id_panneau_modele,marque.id_panneau_marque from mytable
@@ -11,8 +15,12 @@ insert into panneau(id_panneau_modele, id_panneau_marque)
 INSERT INTO installateur (installateur) SELECT distinct installateur FROM mytable;
 
 INSERT INTO onduleur_marque(onduleur_marque) SELECT distinct onduleur_marque from mytable;
+CREATE INDEX onduleur_marque_index
+    ON onduleur_marque(onduleur_marque);
 
 insert into onduleur_modele(onduleur_modele) SELECT DISTINCT mytable.onduleur_modele from mytable;
+CREATE INDEX onduleur_modele_index
+    ON onduleur_modele(onduleur_modele);
 
 INSERT INTO onduleur (id_onduleur_marque, id_onduleur_modele)
 SELECT DISTINCT marque.id_onduleur_marque, modele.id_onduleur_modele
