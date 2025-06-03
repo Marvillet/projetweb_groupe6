18b1a08dd096b1bcb0bfb9eed42997220c92233c
Mathis Letellier <mathis.letellier@isen-ouest.yncrea.fr>
2025-06-03 15:15:06 +0200

    panneau

--- sql/commandInstallation.sql
+++ sql/commandInstallation.sql
@@ -1,2 +1,6 @@
 #-- executer tous les autres sql avant
+INSERT INTO panneaux_marque(panneaux_marque) SELECT distinct panneaux_marque from mytable;
 
+insert into panneaux_modele(panneaux_modele) SELECT DISTINCT mytable.panneaux_modele from mytable;
+
+insert into panneau(id_modele_panneau, id_panneaux_marque) SELECT
