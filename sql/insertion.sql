f91f063cdaf8dffc60c6be3d4ae7fdf8098b2d05
mbross28 <mathis.brosseau@isen-ouest.yncrea.fr>
2025-06-03 15:26:32 +0200

    insertion

--- sql/insertion.sql
+++ sql/insertion.sql
@@ -1,1 +1,5 @@
 INSERT INTO installateur (installateur) SELECT distinct installateur FROM mytable;
+
+INSERT INTO onduleur_marque(onduleur_marque) SELECT distinct onduleur_marque from mytable;
+
+insert into onduleur_modele(onduleur_modele) SELECT DISTINCT mytable.onduleur_modele from mytable;
