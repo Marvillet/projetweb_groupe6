cc6e294d19536cb1e85db1634b50025375bef0fc
Mathis Letellier <mathis.letellier@isen-ouest.yncrea.fr>
2025-06-04 11:31:12 +0200

    API et controleur

---
+++ php/controleur/installation_controleur.php
@@ -0,0 +1,26 @@
+<?php
+switch ($requestMethod) {
+    case 'GET':
+        if ($id) {
+            //on renvoie les datas d'une installation précise
+            $data = 'installation de l\'id id';
+        } else {
+            $data = '100 installations avec moin de data';
+        }
+        break;
+    case 'POST':
+        if (isset($_POST['id']) && isset($_POST['comment'])) {
+            $data = 'on ajoute la data';
+        }
+        break;
+    case 'DELETE':
+        if ($id != NULL) {
+            $data = 'on supprime la data d\' id  ';
+        }
+        break;
+    case 'PUT':
+        parse_str(file_get_contents('php://input'), $_PUT);
+        if (isset($_PUT['comment']) && $id != NULL) {
+            $data = dbModifyComment($db, $login, $id, $_PUT['comment']);
+        }
+}
