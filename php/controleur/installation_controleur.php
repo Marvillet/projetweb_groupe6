1449faeeeb61f238853a5378d8236a5f2a893a36
Mathis Letellier <mathis.letellier@isen-ouest.yncrea.fr>
2025-06-04 15:39:56 +0200

    api
    toutes les commandes Rest sont sur discorde

--- php/controleur/installation_controleur.php
+++ php/controleur/installation_controleur.php
@@ -1,26 +1,50 @@
 <?php
-switch ($requestMethod) {
-    case 'GET':
-        if ($id) {
-            //on renvoie les data d'une installation précise
-            $data = 'installation de l\'id id';
-        } else {
-            $data = '100 installations avec moins de data';
-        }
-        break;
-    case 'POST':
-        if (isset($_POST['id']) && isset($_POST['comment'])) {
-            $data = 'on ajoute la data';
-        }
-        break;
-    case 'DELETE':
-        if ($id != NULL) {
-            $data = 'on supprime la data d\'id ';
-        }
-        break;
-    case 'PUT':
-        parse_str(file_get_contents('php://input'), $_PUT);
-        if (isset($_PUT['comment']) && $id != NULL) {
-            $data = dbModifyComment($db, $login, $id, $_PUT['comment']);
-        }
+//on récupère le modèle
+
+function GestionDemande($db,$method, $id, $data)
+{
+    $result=false;
+    switch ($method) {
+        case 'GET':
+            if ($id) {
+                //on renvoie les result d'une installation précise
+                $result = infosInstall($db,$id);
+            }
+            break;
+        case 'POST':
+            /*
+            if (isset($_POST['id']) && isset($_POST['comment'])) {
+                $result = 'on ajoute la result';
+            }
+            */
+            break;
+        case 'DELETE':
+            if ($id != NULL) {
+                $result = 'on supprime la data d\'id ';
+            }
+            break;
+        case 'PUT':
+            /*
+            if (isset($_PUT['comment']) && $id != NULL) {
+                $data = dbModifyComment($db, $login, $id, $_PUT['comment']);
+            }
+            */
+            break;
+        default:
+            //http_response_code(405);
+            echo json_encode(["error" => "Méthode non autorisee"]);
+    }
+
+    if($result !=false){
+        // Envoie des données au client
+        header('Content-Type: application/json; charset=utf-8');
+        header('Cache-control: no-store, no-cache, must-revalidate');
+        header('Pragma: no-cache');
+        header('HTTP/1.1 200 OK');
+        echo json_encode($result);
+    }
+    else{
+        header('HTTP/1.1 400 Bad Request');
+        echo json_encode(["error" => "Statistique demandee non valide"]);
+    }
 }
---
+++ php/controleur/lieux_controleur.php
@@ -0,0 +1,46 @@
+<?php
+
+
+function GestionDemande($db, $method, $demande, $data)
+{
+    $result = false;
+    switch ($method) {
+        case 'GET':
+            switch ($demande) {
+                case 'departement':
+                    //si une marque à commencé à être taper
+                    if (isset($data['dep'])) {
+                        $result = "bientot les departements commençant par" . $data['dep'];
+                    } else {
+                        //sinon 20 marques hazard
+                        $result = recupDep($db);
+                    }
+                    break;
+
+                case 'coord':
+                    if (isset($data['dep']) && isset($data['annee'])) {
+                        $result = coord($db, $data['dep'], $data['annee']);
+                    }
+                    break;
+                default:
+                    //http_response_code(405);
+                    echo json_encode(["error" => "Méthode non autorisee"]);
+                    break;
+            }
+        //seule la methode Get est autorisé pour les statistiques
+        default:
+            //http_response_code(405);
+            echo json_encode(["error" => "Méthode non autorisee"]);
+    }
+    if ($result != false) {
+        // Envoie des données au client
+        header('Content-Type: application/json; charset=utf-8');
+        header('Cache-control: no-store, no-cache, must-revalidate');
+        header('Pragma: no-cache');
+        header('HTTP/1.1 200 OK');
+        echo json_encode($result);
+    } else {
+        header('HTTP/1.1 400 Bad Request');
+        echo json_encode(["error" => "Aucune data valide à renvoyer"]);
+    }
+}
