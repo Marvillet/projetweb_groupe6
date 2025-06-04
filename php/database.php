29bdd19981b4b5e2f727ba11bd36a13a55e1d3ac
mbross28 <mathis.brosseau@isen-ouest.yncrea.fr>
2025-06-04 11:13:10 +0200

    fonctions bdd

--- php/database.php
+++ php/database.php
@@ -97,7 +97,7 @@ function nbMarquePan($db){
 // Installation par marque d'onduleur
 function installByOnduleur($db,$marque){
   $stmt = $db->prepare("
-    SELECT * 
+    SELECT id,mois_installation,an_installation,nb_panneaux,surface,puissance_crete,lat,lon 
     FROM installation i
     JOIN onduleur o ON o.id_onduleur = i.id_onduleur
     JOIN onduleur_marque om ON o.id_onduleur_marque = om.id_onduleur_marque
@@ -110,7 +110,7 @@ function installByOnduleur($db,$marque){
 
 // Marque ondulateur
 function marqueOnd($db){
-  $stmt = $db->prepare("SELECT om.ondulateur_marque FROM onduleur_marque om");
+  $stmt = $db->prepare("SELECT id_onduleur_marque,ondulateur_marque FROM onduleur_marque om");
   $stmt->execute();
   $result = $stmt->fetch(PDO::FETCH_ASSOC);
   return $result;
@@ -120,7 +120,7 @@ function marqueOnd($db){
 // Installation par marque de panneau
 function installByPanneau($db,$marque){
   $stmt = $db->prepare("
-    SELECT * 
+    SELECT id,mois_installation,an_installation,nb_panneaux,surface,puissance_crete,lat,lon 
     FROM installation i
     JOIN panneau p ON p.id_panneau = i.id_panneau
     JOIN panneau_marque pm ON o.id_panneau_marque = pm.id_panneau_marque
@@ -133,7 +133,7 @@ function installByPanneau($db,$marque){
 
 // Marque panneau
 function marquePan($db){
-  $stmt = $db->prepare("SELECT panneau_marque FROM panneaux_marque ");
+  $stmt = $db->prepare("SELECT id_panneau_marque,panneau_marque FROM panneaux_marque ");
   $stmt->execute();
   $result = $stmt->fetch(PDO::FETCH_ASSOC);
   return $result;
@@ -141,7 +141,7 @@ function marquePan($db){
 
 // Departements
 function recupDep($db){
-  $stmt = $db->prepare("Select dep_nom from departement");
+  $stmt = $db->prepare("Select dep_code,dep_nom from departement");
   $stmt->execute();
   $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
   return $results;
@@ -150,13 +150,58 @@ function recupDep($db){
 // Installation par departement
 function installByDep($db,$dep){
   $stmt = $db->prepare("
-    SELECT * 
+    SELECT id,mois_installation,an_installation,nb_panneaux,surface,puissance_crete,lat,lon
     FROM installation i
     JOIN Commune c ON i.code_insee = c.code_insee
     JOIN departement d ON c.dep_code = d.dep_code
     WHERE d.dep_nom=:dep;
 ");
   $stmt->execute(['dep'=>$dep]);
+  $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+  return $results;
+}
+
+// Récupération infos d'une installation précise
+function infosInstall($db){
+  $stmt = $db->prepare("
+    SELECT 
+    i.*,
+    inst.installateur,
+    c.nom_standard AS commune,
+    c.code_postal,
+    c.population,
+    d.dep_nom AS departement,
+    r.reg_nom AS region,
+    p.country AS pays,
+    pm.panneau_modele,
+    pmq.panneau_marque,
+    om.onduleur_modele,
+    omq.onduleur_marque
+
+FROM installation i
+
+-- Jointure avec l’installateur
+LEFT JOIN installateur inst ON i.id_installateur = inst.id_installateur
+
+-- Jointure avec la commune
+LEFT JOIN commune c ON i.code_insee = c.code_insee
+
+-- Jointure avec le département et la région
+LEFT JOIN departement d ON c.dep_code = d.dep_code
+LEFT JOIN region r ON d.reg_code = r.reg_code
+LEFT JOIN pays p ON r.id_pays = p.id_pays
+
+-- Jointure avec le panneau
+LEFT JOIN panneau pnn ON i.id_panneau = pnn.id_panneau
+LEFT JOIN panneaux_modele pm ON pnn.id_panneau_modele = pm.id_panneau_modele
+LEFT JOIN panneaux_marque pmq ON pnn.id_panneau_marque = pmq.id_panneau_marque
+
+-- Jointure avec l’onduleur
+LEFT JOIN onduleur ond ON i.id_onduleur = ond.id_onduleur
+LEFT JOIN onduleur_modele om ON ond.id_onduleur_modele = om.id_onduleur_modele
+LEFT JOIN onduleur_marque omq ON ond.id_onduleur_marque = omq.id_onduleur_marque 
+");
+  $stmt->execute();
   $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
   return $results;
 }
