45a4bc453d94afcfee36467c404723a8be5bf5b5
mbross28 <mathis.brosseau@isen-ouest.yncrea.fr>
2025-06-04 15:51:58 +0200

    js acceuil

--- js/accueil.js
+++ js/accueil.js
@@ -1,1 +1,11 @@
 'use strict'
+
+function displayInfo(data){
+    let chambre=document.getElementById('nb_install')
+    data.forEach(lst=>{chambre.innerHTML+=data})
+}
+
+function main(){
+    ajaxRequest('GET','php/request.php/stat/total',displayInfo)
+}
+window.addEventListener("DOMContentLoaded",main)
