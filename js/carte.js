85e9f3bfedb4eb382f25024785f06b0eb98b826c
mbross28 <mathis.brosseau@isen-ouest.yncrea.fr>
2025-06-05 11:11:34 +0200

    js carte

--- js/carte.js
+++ js/carte.js
@@ -1,12 +1,6 @@
-'use strict'
-
-function showInfo(){
+function showInfo() {
     const info = document.getElementById('info');
-    if (info.style.display === 'none' || info.style.display === '') {
-        info.style.display = 'block';
-    } else {
-        info.style.display = 'none';
-    }
+    info.style.display = (info.style.display === 'none' || info.style.display === '') ? 'block' : 'none';
 }
 
 function hideInfo() {
@@ -14,25 +8,23 @@ function hideInfo() {
     info.style.display = 'none';
 }
 
-function ajoutCoord(data){
-    // Initialisation de la carte centrée sur la France
+function ajoutCoord(data) {
     const map = L.map('map').setView([46.603354, 1.888334], 6);
 
-    // Couche OpenStreetMap
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         maxZoom: 19,
         attribution: '&copy; <a href="https://openstreetmap.org/copyright">OpenStreetMap</a>'
     }).addTo(map);
 
-    // Marqueur de test
     data.forEach(dt => {
-        L.marker([dt.lat,dt.lon])
+        L.marker([dt.lat, dt.lon])
             .addTo(map)
             .bindPopup('<b>Installation photovoltaïque</b><br>Marseille<br><button class="showInfo" onclick="showInfo()">Voir détail</button>');
-    })
+    });
 }
 
-function main(){
-    ajaxRequest('GET','php/request.php/lieu/coord?dep='+14+'&annee='+2010,ajoutCoord)
+function main() {
+    ajaxRequest('GET','../php/request.php/lieu/coord?dep=14&annee=2010',ajoutCoord)
 }
-window.addEventListener("DOMContentLoaded", main)
+
+window.addEventListener("DOMContentLoaded", main);
