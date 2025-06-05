6bb8756660c3d5fcf4f2439cd3775d6c6a0a5d45
Mathis Letellier <mathis.letellier@isen-ouest.yncrea.fr>
2025-06-05 09:52:12 +0200

    ajout api date

---
+++ php/controleur/date_controleur.php
@@ -0,0 +1,35 @@
+<?php
+function GestionDemande($db, $method, $demande, $data)
+{
+    $result = false;
+    switch ($method) {
+        case 'GET':
+            switch ($demande) {
+                case 'annee':
+                    //si une marque à commencé à être taper
+                    $result = annee($db);
+                    break;
+                default:
+                    //http_response_code(405);
+                    echo json_encode(["error" => "Méthode non autorisee"]);
+                    break;
+            }
+            break;
+        //seule la methode Get est autorisé pour les statistiques
+        default:
+            //http_response_code(405);
+            echo json_encode(["error" => "Methode non autorisee"]);
+            break;
+    }
+    if ($result != false) {
+        // Envoie des données au client
+        header('Content-Type: application/json; charset=utf-8');
+        header('Cache-control: no-store, no-cache, must-revalidate');
+        header('Pragma: no-cache');
+        header('HTTP/1.1 200 OK');
+        echo json_encode($result);
+    } else {
+        header('HTTP/1.1 400 Bad Request');
+        echo json_encode(["error" => "Aucune data valide à renvoyer"]);
+    }
+}
