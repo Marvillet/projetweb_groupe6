9682875cab6834e4e0fbfeecacc7d72f91ee6e29
mbross28 <mathis.brosseau@isen-ouest.yncrea.fr>
2025-06-05 10:36:42 +0200

    màj database.php

--- php/database.php
+++ php/database.php
@@ -51,8 +51,8 @@ function installByRegion($db, $region){
     WHERE r.reg_code=:region;
 ");
   $stmt->execute(['region'=>$region]);
-  $results = $stmt->fetch(PDO::FETCH_ASSOC);
-  return $results;
+  $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+  return $results[0]['total'];
 }
 
 // Nombre d'installations par années et région
