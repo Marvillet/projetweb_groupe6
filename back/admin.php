8c2c3905d0746328f58c80adafd5a7be50752caa
mbross28 <mathis.brosseau@isen-ouest.yncrea.fr>
2025-06-06 15:38:54 +0200

    modif admin php

--- back/admin.php
+++ back/admin.php
@@ -17,31 +17,26 @@
     <link rel="stylesheet" href="../style/admin.css" />
 </head>
 
-<body>
-<!-- NAVIGATION (ton style conservé) -->
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark px-3">
-    <a class="navbar-brand d-flex align-items-center" href="#">
-        <img src="../images/logo.png" alt="Logo" id="logo" style="height: 40px;" class="me-2">
+<body class="bg-light">
+<nav id="navbar">
+    <div>
+        <img src="../images/logo.png" id="logo" alt="ML Photovoltaic Logo">
         <span>ML Photovoltaic</span>
-    </a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarContent">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarContent">
-        <ul class="navbar-nav ms-auto align-items-center gap-2">
-            <li class="nav-item"><a class="nav-link" href="../front/accueil.html">Accueil</a></li>
-            <li class="nav-item"><a class="nav-link" href="../front/recherche.html">Recherche</a></li>
-            <li class="nav-item"><a class="nav-link" href="../front/carte.html">Carte</a></li>
-            <li class="nav-item">
-                <a class="nav-link" href="#" title="Espace admin"><i class="bi bi-person-gear fs-5"></i></a>
-            </li>
-            <li class="nav-item">
-                <a href="logout.php" class="btn btn-danger btn-sm px-3 py-2 rounded-pill">
-                    <i class="bi bi-box-arrow-right me-2"></i> Déconnexion
-                </a>
-            </li>
-        </ul>
     </div>
+
+    <button id="burger"><i class="bi bi-list"></i></button>
+
+    <ul id="nav-links" class="d-flex align-items-center gap-3 list-unstyled m-0">
+        <li><a href="../front/accueil.html" class="windows">Accueil</a></li>
+        <li><a href="../front/recherche.html" class="windows">Recherche</a></li>
+        <li><a href="../front/carte.html" class="windows">Carte</a></li>
+        <li><a href="" id="mainWindow" title="Espace admin"><i class="bi bi-person-gear fs-5.5"></i></a></li>
+        <li>
+            <a href="logout.php" class="btn btn-danger btn-sm d-inline-flex align-items-center px-3 py-2 rounded-pill">
+                <i class="bi bi-box-arrow-right me-2"></i> Déconnexion
+            </a>
+        </li>
+    </ul>
 </nav>
 
 <!-- CONTENU PRINCIPAL -->
@@ -115,6 +110,12 @@
 
 <!-- SCRIPTS -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+<script>
+    // Toggle menu mobile
+    document.getElementById("burger").addEventListener("click", function () {
+        document.getElementById("nav-links").classList.toggle("show");
+    });
+</script>
 <script src="../js/ajax.js"></script>
 <script src="../js/admin.js"></script>
 <script src="../js/delUpt.js"></script>
