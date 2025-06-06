ff1e703bdaa1ee231be3464306e85704858b7b3e
Mathis Letellier <mathis.letellier@isen-ouest.yncrea.fr>
2025-06-06 15:40:06 +0200

    ajout update et delete

---
+++ js/delUpt.js
@@ -0,0 +1,19 @@
+'use strict'
+window.addEventListener("DOMContentLoaded",main);
+
+function main(){
+    console.log("oui");
+}
+function installupdate(event){
+    console.log(event.target.dataset.id);
+    console.log("update");
+    //id de l'installation
+    let id=event.target.dataset.id;
+
+}
+
+function installdelete(event){
+    console.log("delete");
+    let id=event.target.dataset.id;
+    
+}
