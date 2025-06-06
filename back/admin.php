652fbd7a013c2ce9f9dd2b5eb1426d04d741b52f
mbross28 <mathis.brosseau@isen-ouest.yncrea.fr>
2025-06-06 11:58:29 +0200

    carte js

--- back/admin.php
+++ back/admin.php
@@ -18,8 +18,6 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <!-- Bootstrap Icons (pour <i class="bi ...">) -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css" rel="stylesheet">
-    <!-- Notre feuille perso -->
-    <link rel="stylesheet" href="../style/installation_gestion.css">
 </head>
 
 <body class="bg-light">
