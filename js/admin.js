1a8083201ec829f41896c143ff8f9ab6715379ae
Mathis Letellier <mathis.letellier@isen-ouest.yncrea.fr>
2025-06-07 15:52:52 +0200

    ;

--- js/admin.js
+++ js/admin.js
@@ -61,7 +61,7 @@ function main(){
 }
 
 function addlignes(rows){
-    tbody=document.getElementById("resultat");
+    let tbody=document.getElementById("resultat");
     tbody.innerHTML = "";
     rows.forEach((row) => {
         const tr = document.createElement("tr");
@@ -72,16 +72,16 @@ function addlignes(rows){
               <td>${row['nom_standard']}</td>
               <td>${row['mois_installation']} / ${row['an_installation']}</td>
               <td class="text-center">
-                <button class="btn btn-sm btn-outline-primary me-2" data-action="edit" data-id="${row.id}"><i class="bi bi-pencil"></i></button>
-                <button class="btn btn-sm btn-outline-danger" data-action="delete" data-id="${row.id}"><i class="bi bi-trash"></i></button>
+                <button class="btn btn-sm btn-outline-primary me-2" data-action="edit" data-id="${row['id']}"><i class="bi bi-pencil"></i></button>
+                <button class="btn btn-sm btn-outline-danger" data-action="delete" data-id="${row['id']}"><i class="bi bi-trash"></i></button>
               </td>
             `;
         tr.getElementsByClassName('btn-outline-primary')[0].addEventListener("click",installupdate);
-        tr.getElementsByClassName('btn-outline-danger')[0].addEventListener("click",installdelete)
+        tr.getElementsByClassName('btn-outline-danger')[0].addEventListener("click",installdelete);
         tbody.appendChild(tr);
     });
     window.displayPage(1)
-};
+}
 function affichetoast(toastEl,message, error = false){
     toastEl.classList.remove("bg-success", "bg-danger");
     toastEl.classList.add(error ? "bg-danger" : "bg-success");
