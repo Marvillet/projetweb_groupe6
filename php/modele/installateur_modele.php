7c9f1981331d0bbe2f70a417ec4af5a0a3edf099
Mathis Letellier <mathis.letellier@isen-ouest.yncrea.fr>
2025-06-07 17:42:50 +0200

    update installateur
    Co-Authored-By: mbross28 <148534966+mbross28@users.noreply.github.com>

--- php/modele/installateur_modele.php
+++ php/modele/installateur_modele.php
@@ -11,8 +11,9 @@ class installateur
     }
 //Récupère 20 departement de panneau commençant par $text
     static function installateurFiltre($db,$text){
+        //installateurs car installateur mot clés js pour empecher bug
         $stmt = $db->prepare("
-         SELECT id_installateur,installateur.installateur 
+         SELECT id_installateur,installateur.installateur as installateurs
          FROM installateur 
          WHERE LOWER(installateur.installateur) LIKE LOWER(:term) LIMIT 20 ;
     ");
